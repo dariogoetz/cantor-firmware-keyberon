ab31b459e7198897c93864144c7dbc72c1677d74
Dario Götz <dario.goetz@googlemail.com>
2022-06-10 17:07:25 +0200

    [mod] update comments for DirectPinMatrix

--- src/direct_pin_matrix.rs
+++ src/direct_pin_matrix.rs
@@ -2,15 +2,10 @@
 
 use embedded_hal::digital::v2::InputPin;
 
-/// Describes the hardware-level matrix of switches.
+/// Matrix-representation of switches directly attached to the pins ("diodeless").
 ///
 /// Generic parameters are in order: The type of column pins,
-/// the type of row pins, the number of columns and rows.
-/// **NOTE:** In order to be able to put different pin structs
-/// in an array they have to be downgraded (stripped of their
-/// numbers etc.). Most HAL-s have a method of downgrading pins
-/// to a common (erased) struct. (for example see
-/// [stm32f0xx_hal::gpio::PA0::downgrade](https://docs.rs/stm32f0xx-hal/0.17.1/stm32f0xx_hal/gpio/gpioa/struct.PA0.html#method.downgrade))
+/// the number of columns and rows.
 pub struct DirectPinMatrix<P, const CS: usize, const RS: usize>
 where
     P: InputPin,
@@ -24,8 +19,7 @@ where
 {
     /// Creates a new DirectPinMatrix.
     ///
-    /// Assumes columns are pull-up inputs,
-    /// and rows are output pins which are set high when not being scanned.
+    /// Assumes pins are pull-up inputs,
     pub fn new<E>(pins: [[Option<P>; CS]; RS]) -> Result<Self, E>
     where
         P: InputPin<Error = E>,
@@ -34,10 +28,7 @@ where
         Ok(res)
     }
 
-    /// Scans the matrix and checks which keys are pressed.
-    ///
-    /// Every row pin in order is pulled low, and then each column
-    /// pin is tested; if it's low, the key is marked as pressed.
+    /// Scans the pins and checks which keys are pressed (state is "low").
     pub fn get<E>(&mut self) -> Result<[[bool; CS]; RS], E>
     where
         P: InputPin<Error = E>,
@@ -45,7 +36,7 @@ where
         let mut keys = [[false; CS]; RS];
 
         for (ri, row) in (&mut self.pins).iter_mut().enumerate() {
-            for (ci, col_option) in (&row).iter().enumerate() {
+            for (ci, col_option) in row.iter().enumerate() {
                 if let Some(col) = col_option {
                     if col.is_low()? {
                         keys[ri][ci] = true;
