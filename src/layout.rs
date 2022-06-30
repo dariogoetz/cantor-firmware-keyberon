32ff994144d94d6af4cf433a926ab7148895317b
Dario Götz <dario.goetz@googlemail.com>
2022-06-30 10:07:50 +0200

    [mod] thumbs transparent on Layer 4

--- src/layout.rs
+++ src/layout.rs
@@ -67,7 +67,7 @@ pub static LAYERS: keyberon::layout::Layers<12, 4, 5, ()> = keyberon::layout::la
         [ t  PgUp   BSpace Up   Delete PgDown n      Kb7 Kb8 Kb9 RBracket       Slash],
         [(3) Home   Left   Down Right  End    n      Kb4 Kb5 Kb6 Comma          Dot],
         [ t  Escape Tab    n    Enter  n      Kb0    Kb1 Kb2 Kb3 {s!(RBracket)} {s!(Kb7)}],
-        [ t  t      t      t    t      t      LShift t   t   t   t              t],
+        [ t  t      t      t    t      t      t      t   t   t   t              t],
     }{
         [{Custom(())}  n    n     n     VolUp    n   F12  F7  F8  F9  n  {Custom(())}],
         [t             n    n     n     {PPN}    n   F11  F4  F5  F6  n  t],
