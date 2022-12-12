bed9278bdc3c06806762ce9087c1ccfd8890ea0e
Dario Götz <dario.goetz@googlemail.com>
2022-12-12 17:20:59 +0100

    [mod] noted variant

--- src/layout.rs
+++ src/layout.rs
@@ -54,9 +54,9 @@ macro_rules! a {
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers<12, 4, 5, ()> = keyberon::layout::layout! {
     {
-        [ J     Y     W     U     A     Q     P     F     M     L     Z       -  ],
+        [ J     Y     Z     U     A     Q     P     B     M     L     F       -  ],
         [(1)    C     S     I     E     O     D     T     N     R     H      (1) ],
-        [LGui   V     X LBracket Quote  ;     B     G     ,     .     K      LGui],
+        [LGui   V     X LBracket Quote  ;     W     G     ,     .     K      LGui],
         [ t     t t (2) LShift {CTRL_TAB} {ALT_ENT} {SHIFT_SP} (2) t  t      t   ],
     }{
         [ t {a!(E)}     {s!(Slash)} {a!(Kb8)}         {a!(Kb9)}      Grave          {s!(Kb1)}   NonUsBslash {s!(NonUsBslash)} {s!(Kb0)}       {s!(Kb6)}   {a!(Q)} ],
